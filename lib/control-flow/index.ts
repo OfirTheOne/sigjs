188bfe7631b8e9a73671821d50693204c380d322
OfirTheOne <og8217@intl.att.com>
2024-01-24 22:53:45 +0200

    Add Await components to control-flow module

--- lib/control-flow/index.ts
+++ lib/control-flow/index.ts
@@ -4,3 +4,5 @@ export { If } from './if';
 export type { IfProps } from './if';
 export { For } from './for';
 export type { ForProps } from './for';
+export { Await } from './await';
+export type { AwaitProps } from './await';
