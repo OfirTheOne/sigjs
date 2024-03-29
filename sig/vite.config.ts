36524a7e2eb138772546306d90e10e4f03d34534
OfirTheOne <og8217@intl.att.com>
2024-03-29 23:30:33 +0300

    rename @sig to sig

--- sig/vite.config.ts
+++ sig/vite.config.ts
@@ -3,18 +3,15 @@ import dts from 'vite-plugin-dts';
 import path from 'path';
 import pkg from './package.json';
 
-// Use an environment variable to switch between configurations
-const isLibBuild = process.env.BUILD_TARGET === 'lib';
-
 export default defineConfig({
-  root: isLibBuild ? '.' : './app',
+  root: '.' ,
   build: {
     sourcemap: true,
-    lib: isLibBuild ? {
+    lib: {
       entry: path.resolve(__dirname, "lib/index.ts"),
       name: pkg.name,
       fileName: "index",
-    } : undefined,
+    },
   },
   resolve: {
     alias: {
