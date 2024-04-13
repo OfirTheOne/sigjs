2b25a7f049eb9072b9557130ca107dcc46dd01b8
OfirTheOne <og8217@intl.att.com>
2024-04-13 19:44:40 +0300

    fix RAW_ELEMENT render handling

--- sig/lib/jsx/create-element.ts
+++ sig/lib/jsx/create-element.ts
@@ -2,13 +2,26 @@
 
 import { createElement as renderCreateElement } from "../core/dom-render/create-element";
 import * as elements from "@/convenient/element";
-import { VirtualElementChild } from "@/types";
+import { VirtualElementChild, VirtualElement, ELEMENT_TYPE } from "@/types";
 
 export function createElement(
     tag: string | ((...args: unknown[]) => unknown),
     props: { [key: string]: unknown },
     ...children: VirtualElementChild[]
 ) {
+    children = children.map(child => {
+        if (child instanceof HTMLElement) {
+            return (<VirtualElement>{
+                type: ELEMENT_TYPE.RAW,
+                props: {
+                    rawElement: child,
+                    children: []
+                }
+            });     
+        }
+        return child;
+    });
+
     if (typeof tag === 'function') {
         return tag(props, children);
     }
