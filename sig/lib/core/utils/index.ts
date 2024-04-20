85eb103cbe9e437269f57a65c7510040cd5d29bf
OfirTheOne <og8217@intl.att.com>
2024-04-20 21:26:43 +0300

    Add isNodeHTMLElement and isNodeText functions to index.ts

--- sig/lib/core/utils/index.ts
+++ sig/lib/core/utils/index.ts
@@ -1,5 +1,12 @@
 import { VirtualElement } from "@/types";
 
+export function isNodeHTMLElement(node: Node): node is HTMLElement {
+  return node && node.nodeType === Node.ELEMENT_NODE;
+}
+
+export function isNodeText(node: Node): node is Text {
+  return node && node.nodeType === Node.TEXT_NODE;
+}
 
 
 export function isVirtualElement(value: unknown): value is VirtualElement {
