13973e6376656a035a5dacfd0f66a89d1ff2743d
OfirTheOne <og8217@intl.att.com>
2024-05-11 19:40:55 +0300

    Refactor appendChild method in html.ts to support appending multiple child nodes

---
+++ sig/lib/common/logger/logger.ts
@@ -0,0 +1,29 @@
+class Logger {
+    private debugMode: boolean;
+    private static instance: Logger;
+
+    private constructor(debugMode: boolean) {
+        this.debugMode = debugMode;
+    }
+
+    static getInstance(debugMode: boolean): Logger {
+        if (!Logger.instance) {
+            Logger.instance = new Logger(debugMode);
+        }
+        return Logger.instance;
+    }
+
+    log(message: string): void {
+        if (this.debugMode) {
+            console.log(message);
+        }
+    }
+    
+    warn(message: string): void {
+        if (this.debugMode) {
+            console.warn(message);
+        }
+    }
+}
+
+export default Logger.getInstance(true);
