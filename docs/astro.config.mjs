6b1e29664f6f301c8f81fd851dabb04cf0af5b68
og8217 <og8217@intl.att.com>
2025-01-10 22:42:30 +0200

    add expressive-code

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,88 +2,96 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { createStarlightTypeDocPlugin } from 'starlight-typedoc'
+import expressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 // https://astro.build/config
 const [createDocumentation, documentation] = createStarlightTypeDocPlugin();
-// const [createTransformersDocumentation, transformersDocumentationSidebar] = createStarlightTypeDocPlugin();
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			plugins: [
-				createDocumentation({
-					entryPoints: [
-					  '../sig/lib/index.ts',
-					  '../sig/lib/router',
-					  '../sig/lib/store',
-					],
-					tsconfig: '../sig/tsconfig.json',
-					typeDoc:{
-						"navigation": {
-							"includeCategories": false,
-							"includeFolders": false,
-							"includeGroups": false,
-						},
-						"exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
-						"excludeExternals": true,
-						"excludePrivate": true,
-						"excludeProtected": true,
-						"excludeInternal": true,
-						"excludeNotDocumented": true,
-						"compilerOptions": {
-							"excludeInternal": true,
-						},
-						"hideGenerator": true,
-						"groupReferencesByType": false,
-						"categorizeByGroup": false,
-						"readme": "none",
-					}
-					
-				}),
-			  ],
-			sidebar: [
-				{
-					label: "Documentation",
-					items: [documentation],
-					
-				},
-				{
-					label: 'Guides',
-					items: [
-						{
-							label: 'Components',
-							slug: 'guides/components',
-						},
-						{
-							label: 'Lifecycle',
-							slug: 'guides/lifecycle',
-						},
-						{
-							label: 'Signals',
-							slug: 'guides/signals',
-						},
-						{
-							label: 'Rendering Control Flow', 
-							slug: 'guides/example' 
-						},
-						{
-							label: 'Routing',
-							slug: 'guides/example'
-						},
-						{
-							label: 'Stores',
-							slug: 'guides/example'
-						},
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    
+    integrations: [
+        expressiveCode({
+            plugins: [pluginLineNumbers()],
+        }),
+        starlight({
+        title: 'My Docs',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        customCss: [
+            '/src/styles/custom.css',
+          ],
+        plugins: [
+            createDocumentation({
+                entryPoints: [
+                  '../sig/lib/index.ts',
+                  '../sig/lib/router',
+                  '../sig/lib/store',
+                ],
+                tsconfig: '../sig/tsconfig.json',
+                typeDoc:{
+                    "navigation": {
+                        "includeCategories": false,
+                        "includeFolders": false,
+                        "includeGroups": false,
+                    },
+                    "exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
+                    "excludeExternals": true,
+                    "excludePrivate": true,
+                    "excludeProtected": true,
+                    "excludeInternal": true,
+                    "excludeNotDocumented": true,
+                    "compilerOptions": {
+                        "excludeInternal": true,
+                    },
+                    // "useCodeBlocks": true,
+                    "hideGenerator": true,
+                    "groupReferencesByType": false,
+                    "categorizeByGroup": false,
+                    "readme": "none",
+                }
+                
+            }),
+          ],
+        sidebar: [
+            {
+                label: "Documentation",
+                items: [documentation],
+                
+            },
+            {
+                label: 'Guides',
+                items: [
+                    {
+                        label: 'Components',
+                        slug: 'guides/components',
+                    },
+                    {
+                        label: 'Lifecycle',
+                        slug: 'guides/lifecycle',
+                    },
+                    {
+                        label: 'Signals',
+                        slug: 'guides/signals',
+                    },
+                    {
+                        label: 'Rendering Control Flow', 
+                        slug: 'guides/example' 
+                    },
+                    {
+                        label: 'Routing',
+                        slug: 'guides/example'
+                    },
+                    {
+                        label: 'Stores',
+                        slug: 'guides/example'
+                    },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    })],
 });
