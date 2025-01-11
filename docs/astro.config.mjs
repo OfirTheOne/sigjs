10e683d00a06ac3e1ec151224ea951a247cf6035
og8217 <og8217@intl.att.com>
2025-01-11 20:55:59 +0200

    update documentation configuration and sidebar for new guides

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,90 +8,94 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 const [createDocumentation, documentation] = createStarlightTypeDocPlugin();
 
 export default defineConfig({
-    
     integrations: [
         expressiveCode({
             plugins: [pluginLineNumbers()],
         }),
         starlight({
-        title: 'My Docs',
-        social: {
-            github: 'https://github.com/withastro/starlight',
-        },
-        customCss: [
-            '/src/styles/custom.css',
-          ],
-        plugins: [
-            createDocumentation({
-                entryPoints: [
-                  '../sig/lib/index.ts',
-                  '../sig/lib/router',
-                  '../sig/lib/store',
-                ],
-                tsconfig: '../sig/tsconfig.json',
-                typeDoc:{
-                    "navigation": {
-                        "includeCategories": false,
-                        "includeFolders": false,
-                        "includeGroups": false,
-                    },
-                    "exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
-                    "excludeExternals": true,
-                    "excludePrivate": true,
-                    "excludeProtected": true,
-                    "excludeInternal": true,
-                    "excludeNotDocumented": true,
-                    "compilerOptions": {
+            title: 'My Docs',
+            social: {
+                github: 'https://github.com/withastro/starlight',
+            },
+            customCss: [
+                '/src/styles/custom.css',
+            ],
+            plugins: [
+                createDocumentation({
+                    entryPoints: [
+                        '../sig/lib/index.ts',
+                        '../sig/lib/router',
+                        '../sig/lib/store',
+                    ],
+                    tsconfig: '../sig/tsconfig.json',
+                    typeDoc: {
+                        "gitRemote": "https://github.com/OfirTheOne/sigjs",
+                        "navigation": {
+                            "includeCategories": false,
+                            "includeFolders": false,
+                            "includeGroups": false,
+                        },
+                        "exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
+                        "excludeExternals": true,
+                        "excludePrivate": true,
+                        "excludeProtected": true,
                         "excludeInternal": true,
-                    },
-                    // "useCodeBlocks": true,
-                    "hideGenerator": true,
-                    "groupReferencesByType": false,
-                    "categorizeByGroup": false,
-                    "readme": "none",
-                }
-                
-            }),
-          ],
-        sidebar: [
-            {
-                label: "Documentation",
-                items: [documentation],
-                
-            },
-            {
-                label: 'Guides',
-                items: [
-                    {
-                        label: 'Components',
-                        slug: 'guides/components',
-                    },
-                    {
-                        label: 'Lifecycle',
-                        slug: 'guides/lifecycle',
-                    },
-                    {
-                        label: 'Signals',
-                        slug: 'guides/signals',
-                    },
-                    {
-                        label: 'Rendering Control Flow', 
-                        slug: 'guides/example' 
-                    },
-                    {
-                        label: 'Routing',
-                        slug: 'guides/example'
-                    },
-                    {
-                        label: 'Stores',
-                        slug: 'guides/example'
-                    },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-    })],
+                        "excludeNotDocumented": true,
+                        "compilerOptions": {
+                            "excludeInternal": true,
+                        },
+                        // "useCodeBlocks": true,
+                        "hideGenerator": true,
+                        "groupReferencesByType": false,
+                        "categorizeByGroup": false,
+                        "readme": "none",
+                    }
+
+                }),
+            ],
+            sidebar: [
+                {
+                    label: "Documentation",
+                    items: [documentation],
+
+                },
+                {
+                    label: 'Guides',
+                    items: [
+                        {
+                            label: 'Components',
+                            slug: 'guides/components',
+                        },
+                        {
+                            label: 'Rendering',
+                            slug: 'guides/rendering',
+                        },
+                        {
+                            label: 'Lifecycle',
+                            slug: 'guides/lifecycle',
+                        },
+                        {
+                            label: 'Signals',
+                            slug: 'guides/signals',
+                        },
+                        {
+                            label: 'Dynamic Rendering',
+                            slug: 'guides/dynamic-rendering'
+                        },
+                        {
+                            label: 'Routing',
+                            slug: 'guides/routing'
+                        },
+                        {
+                            label: 'Store',
+                            slug: 'guides/store'
+                        },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        })],
 });
