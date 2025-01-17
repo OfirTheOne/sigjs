41987df7e4f3c81afa5399d34edaef51da92c4a9
OfirTheOne <og8217@intl.att.com>
2025-01-17 02:22:02 +0200

    add tailwind

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,125 +7,123 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
 const [createDocumentation, documentation] = createStarlightTypeDocPlugin();
 
 export default defineConfig({
     site: 'https://github.com/OfirTheOne/sigjs',
 
     // base: 'my-repo',
-    integrations: [
-        expressiveCode({
-            plugins: [pluginLineNumbers()],
-        }),
-        mdx(),
-        starlight({
-            title: 'Sig',
-            customCss: [
-                '/src/styles/custom.css',
-            ],
-            plugins: [
-                createDocumentation({
-                    entryPoints: [
-                        '../sig/lib/index.ts',
-                        '../sig/lib/router',
-                        '../sig/lib/store',
-                    ],
-                    tsconfig: '../sig/tsconfig.json',
-                    typeDoc: {
-                        "gitRemote": "https://github.com/OfirTheOne/sigjs",
-                        "navigation": {
-                            "includeCategories": false,
-                            "includeFolders": false,
-                            "includeGroups": false,
-                        },
-                        "exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
-                        "excludeExternals": true,
-                        "excludePrivate": true,
-                        "excludeProtected": true,
+    integrations: [expressiveCode({
+        plugins: [pluginLineNumbers()],
+    }), mdx(), starlight({
+        title: 'Sig.JS',
+        customCss: [
+            '/src/styles/custom.css',
+        ],
+        plugins: [
+            createDocumentation({
+                entryPoints: [
+                    '../sig/lib/index.ts',
+                    '../sig/lib/router',
+                    '../sig/lib/store',
+                ],
+                tsconfig: '../sig/tsconfig.json',
+                typeDoc: {
+                    "gitRemote": "https://github.com/OfirTheOne/sigjs",
+                    "navigation": {
+                        "includeCategories": false,
+                        "includeFolders": false,
+                        "includeGroups": false,
+                    },
+                    "exclude": ["**/__tests__/**", "**/*.spec.ts", "**/*.test.ts"],
+                    "excludeExternals": true,
+                    "excludePrivate": true,
+                    "excludeProtected": true,
+                    "excludeInternal": true,
+                    "excludeNotDocumented": true,
+                    "compilerOptions": {
                         "excludeInternal": true,
-                        "excludeNotDocumented": true,
-                        "compilerOptions": {
-                            "excludeInternal": true,
-                        },
-                        // "useCodeBlocks": true,
-                        "hideGenerator": true,
-                        "groupReferencesByType": false,
-                        "categorizeByGroup": false,
-                        "readme": "none",
-                    }
+                    },
+                    // "useCodeBlocks": true,
+                    "hideGenerator": true,
+                    "groupReferencesByType": false,
+                    "categorizeByGroup": false,
+                    "readme": "none",
+                }
 
-                }),
-            ],
-            sidebar: [
-                {
-                    label: "Documentation",
-                    items: [documentation],
+            }),
+        ],
+        sidebar: [
+            {
+                label: "Documentation",
+                items: [documentation],
 
-                },
-                {
-                    label: 'Guides',
-                    items: [
-                        {
-                            label: 'Components',
-                            slug: 'guides/components',
-                        },
-                        {
-                            label: 'Rendering',
-                            slug: 'guides/rendering',
-                        },
-                        {
-                            label: 'Lifecycle',
-                            slug: 'guides/lifecycle',
-                        },
-                        {
-                            label: 'Signals',
-                            slug: 'guides/signals',
-                        },
-                        {
-                            label: 'Dynamic Rendering',
-                            slug: 'guides/dynamic-rendering'
-                        },
-                        {
-                            label: 'Routing',
-                            slug: 'guides/routing'
-                        },
-                        {
-                            label: 'Store',
-                            slug: 'guides/store'
-                        },
-                    ],
-                },
-                {
-                    label: 'Routing Guides',
-                    items: [
-                        {
-                            label: 'Configure Routes',
-                            slug: 'routing-guides/configure-routes'
-                        },
-                        {
-                            label: 'Nested Routes',
-                            slug: 'routing-guides/route'
-                        },
-                        {
-                            label: 'Router Outlet',
-                            slug: 'routing-guides/router-outlet'
-                        },
-                        {
-                            label: 'Redirect',
-                            slug: 'routing-guides/redirect'
-                        },
-                        {
-                            label: 'Link',
-                            slug: 'routing-guides/link'
-                        },
-                    ]
-                },
-                {
-                    label: 'Reference',
-                    autogenerate: { directory: 'reference' },
-                },
-            ],
-        })],
+            },
+            {
+                label: 'Guides',
+                items: [
+                    {
+                        label: 'Components',
+                        slug: 'guides/components',
+                    },
+                    {
+                        label: 'Rendering',
+                        slug: 'guides/rendering',
+                    },
+                    {
+                        label: 'Lifecycle',
+                        slug: 'guides/lifecycle',
+                    },
+                    {
+                        label: 'Signals',
+                        slug: 'guides/signals',
+                    },
+                    {
+                        label: 'Dynamic Rendering',
+                        slug: 'guides/dynamic-rendering'
+                    },
+                    {
+                        label: 'Routing',
+                        slug: 'guides/routing'
+                    },
+                    {
+                        label: 'Store',
+                        slug: 'guides/store'
+                    },
+                ],
+            },
+            {
+                label: 'Routing Guides',
+                items: [
+                    {
+                        label: 'Configure Routes',
+                        slug: 'routing-guides/configure-routes'
+                    },
+                    {
+                        label: 'Nested Routes',
+                        slug: 'routing-guides/route'
+                    },
+                    {
+                        label: 'Router Outlet',
+                        slug: 'routing-guides/router-outlet'
+                    },
+                    {
+                        label: 'Redirect',
+                        slug: 'routing-guides/redirect'
+                    },
+                    {
+                        label: 'Link',
+                        slug: 'routing-guides/link'
+                    },
+                ]
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    }), tailwind()],
 
     adapter: netlify(),
 });
