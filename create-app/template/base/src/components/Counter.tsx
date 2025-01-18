c5ec2f4d1c1d3cdd5db413321cee78158e60a5fa
OfirTheOne <og8217@intl.att.com>
2025-01-19 01:47:06 +0200

    feat: implement enhance Counter component with animations

--- create-app/template/base/src/components/Counter.tsx
+++ create-app/template/base/src/components/Counter.tsx
@@ -1,17 +1,35 @@
 
+import "./Counter.scss";
 import { createSignal } from "@sigjs/sig";
+import Logo from "/assets/sig-logo.svg";
 
 export function Counter() {
     const [count$, setCount] = createSignal(0);
+    const[animation$, setAnimation]= createSignal('');
+
+    const handleCount = (increment: boolean) => {
+      setAnimation(increment ? 'bounce-up' : 'bounce-down');
+      setCount(prev => prev + (increment ? 1 : -1));
+      setTimeout(() => setAnimation(''), 300);
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center gap-2">
-            <div>{count$}</div>
-            <button
-                onClick={() => setCount((count) => count + 1)}
-                className="p-2 bg-indigo-600 text-white rounded-md"
-            >
-                Increment
-            </button>
+        <div className="container">
+            <div className="logo-container">
+                <img src={Logo} alt="Sig.JS Logo" />
+                <h1 className="title">ig.JS</h1>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2">
+                <div className="counter-card">
+                    <h2>Interactive Counter</h2>
+                    <div className={['counter-value', animation$]}>{count$}</div>
+                    <div className="counter-buttons">
+                        <button onClick={() => handleCount(false)}>Decrement</button>
+                        <button onClick={() => handleCount(true)}> Increment </button>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
