9443be8d2d43976b996f56d418aeaf11ccf9dcd4
OfirTheOne <ofir3322@gmail.com>
2025-01-22 22:19:47 +0200

    fix: update deploy script to check for changes in the entire repository instead of just the docs folder

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Change to the script's directory
+cd "$SCRIPT_DIR"
 
 # Check for changes in the docs folder
-if git diff --quiet --exit-code -- docs; then
+if git diff --quiet --exit-code -- .; then
   echo "No changes in the docs folder. Proceeding with deployment..."
   npm run build
   npm run deploy
