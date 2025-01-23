87d89c29f1f829b7be4bae9cb114b6546327cd13
OfirTheOne <ofir3322@gmail.com>
2025-01-23 17:00:38 +0200

    feat: add isExactMatch property to MatchResult interface and update match function

--- sig/lib/router/match-algorithm/match.ts
+++ sig/lib/router/match-algorithm/match.ts
@@ -5,11 +5,13 @@ import { matchRoutePaths, splitPath } from './nested-match-route'
  * The result of a match
  * @property {boolean} isMatch Whether the path matched the route path
  * @property {Record<string, string>} params The parameters extracted from the path
+ * @property {boolean} isExactMatch Whether the path is an exact match
  * 
  * used in {@link match}
  */
 export interface MatchResult {
     isMatch: boolean;
+    isExactMatch: boolean;
     params?: Record<string, string>;
 }
 
@@ -23,13 +25,14 @@ export interface MatchResult {
  * using {@link MatchResult}
  * 
  * @example
- * match('/user/123/profile', '/user/:id/profile') // { isMatch: true, params: { id: '123' } }
- * match('/user/123', '/user') // { isMatch: false }
+ * match('/user/:id/profile', '/user/123/profile') // { isMatch: true, params: { id: '123' } }
+ * match('/user/:id', '/user') // { isMatch: false }
+ * match('/user', '/user/123') // { isMatch: true } 
  * 
  */
 export function match(toMatch: string, matchTo: string): MatchResult {
     const toMatchParts = splitPath(toMatch);
     const matchToParts = splitPath(matchTo);
-    const { isMatch, params } = matchRoutePaths(toMatchParts, matchToParts);
-    return { isMatch, params };
+    const { isMatch, params, isExactMatch = false } = matchRoutePaths(toMatchParts, matchToParts);
+    return { isMatch, params, isExactMatch };
 }
