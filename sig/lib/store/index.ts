fd71e3a90dd1b9968ddb8536fb3993463c07ef35
OfirTheOne <ofir3322@gmail.com>
2025-02-10 22:37:15 +0200

    moving all types to types file

--- sig/lib/store/index.ts
+++ sig/lib/store/index.ts
@@ -1,2 +1,3 @@
 export * from './store';
 export * from './comparator';
+export * from './types';
---
+++ sig/lib/store/types.ts
@@ -0,0 +1,40 @@
+import type { Signal } from "@/core/signal";
+
+export interface Comparator<T> {
+    (prev: T, next: T): boolean;
+}
+
+export interface Selector<T, U> {
+    (state: T): U;
+}
+
+export interface Middleware<T> {
+    (newState: T, state: Partial<T>, store: IStore<T>): void;
+}
+
+export interface StoreOptions<T> { }
+
+export interface IStore<T> {
+    getState(): T;
+    setState(newStateOrSetter: Partial<T> | ((state: T) => Partial<T>)): void;
+    select<U>(selector: Selector<T, U>, comparator?: Comparator<U>): Signal<U>;
+    subscribe<U>(selector: Selector<T, U>, listener: (value: U) => void, comparator?: Comparator<U>): void;
+    notify(): void;
+}
+
+/**
+ * utility type to infer the state type of a store
+ */
+export type InferStoreType<T> = T extends IStore<infer U> ? U : never;
+
+/**
+ * utility type to extract function properties from an object type
+ */
+type FunctionProperties<T> = {
+    [K in keyof T as T[K] extends (...args: any[]) => any ? K : never]: T[K];
+};
+
+export type StoreWithFunctions<T> = IStore<T> & FunctionProperties<T>;
+
+
+
