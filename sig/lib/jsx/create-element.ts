587564b459ce89d42d56e925443179de1f1521df
OfirTheOne <ofir3322@gmail.com>
2025-02-12 00:36:11 +0200

    refactor(create-element): simplify child handling by removing unnecessary raw element creation

--- sig/lib/jsx/create-element.ts
+++ sig/lib/jsx/create-element.ts
@@ -1,9 +1,7 @@
 
 
 import { createElement as renderCreateElement } from "@/core/dom-render/create-element";
-import { createRawElement } from "@/core/dom-render/create-element/element-factory";
 import { adaptVirtualElementChild } from "@/core/dom-render/create-element/adapt-virtual-element-child";
-import { isNodeElement, isNodeText } from "@/core/utils";
 import { createFragment } from "./create-fragment";
 import type { Renderable, VirtualElement, ComponentFunction } from "@/types";
 
@@ -16,12 +14,5 @@ export function createElement(
         const fragmentList = createFragment(null, props, ...children) as VirtualElement[];
         return fragmentList.map(adaptVirtualElementChild);
     }
-
-    const virtualChildren = children.map((child) => {
-        if (isNodeElement(child) || isNodeText(child)) {
-            return createRawElement(child, props, ...children);
-        }
-        return child;
-    });
-    return renderCreateElement(tag, props, ...virtualChildren);
+    return renderCreateElement(tag, props, ...children);
 }
