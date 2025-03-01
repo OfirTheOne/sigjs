944de805a32c9120a6e849b3498465cb05957e12
OfirTheOne <ofir3322@gmail.com>
2025-03-01 18:55:17 +0200

    fix: update handleRoutesMatchedResult to correctly retrieve child nodes when rendering matches

--- sig/lib/router/build-router/handle-routes-matched-result.ts
+++ sig/lib/router/build-router/handle-routes-matched-result.ts
@@ -77,6 +77,9 @@ export async function handleRoutesMatchedResult(
             };
             const componentVirtualElement = createElement(routeSync.component, routeComponentProps);
             currentComponentDom = render(componentVirtualElement, componentContainer, currentRouteRenderKey.push(routeId));
+            if(currentComponentDom === componentContainer) {
+                currentComponentDom = DOM.getChildNodes(componentContainer);
+            }
             memoRenderedRoute[routeId] = currentComponentDom;
             renderMatchResult = {
                 routeId,
