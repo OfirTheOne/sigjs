bccf0d89178bd0832dabb017b66a6c1ecb56a271
OfirTheOne <ofir3322@gmail.com>
2025-03-08 20:49:36 +0200

    feat: add app configuration module with default settings

---
+++ sig/lib/common/app-config/app-config.ts
@@ -0,0 +1,8 @@
+
+const providedAppConfig = globalThis?.env?.SIGJS_APP_CONFIG ? JSON.parse(globalThis.env.SIGJS_APP_CONFIG) : {};
+
+
+export const appConfig = Object.freeze({
+  logLevel: (providedAppConfig.logLevel as string) || 'info',
+  memoize: 'memoize' in providedAppConfig ? (providedAppConfig.memoize as boolean) : true,
+});
