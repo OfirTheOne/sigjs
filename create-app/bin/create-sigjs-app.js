ef82a291fec26c762ef6487dc9bcf4f5a896b0bd
OfirTheOne <ofir3322@gmail.com>
2025-03-15 15:39:19 +0200

    Add Tailwind CSS integration to create-sigjs-app

--- create-app/bin/create-sigjs-app.js
+++ create-app/bin/create-sigjs-app.js
@@ -2,7 +2,7 @@
 
 const { prompt } = require('inquirer');
 const { join } = require('path');
-const { copy } = require('fs-extra');
+const { readFile, writeFile, copy } = require('fs-extra');
 
 const ignoredFiles = [
   'node_modules',
@@ -11,6 +11,42 @@ const ignoredFiles = [
   '.git',
   '.DS_Store'
 ];
+
+async function addTailwindToScss(projectPath) {
+  const indexScssPath = join(projectPath, 'src/index.scss');
+  const tailwindImport = '@use "tailwindcss";\n';
+  try {
+    const existingContent = await readFile(indexScssPath, 'utf8');
+    if (!existingContent.includes(tailwindImport)) {
+      await writeFile(indexScssPath, tailwindImport + existingContent);
+    } else {
+      console.log('Tailwind CSS already present in index.scss');
+    }
+  } catch (err) {
+    console.error('Error adding Tailwind CSS to index.scss:', err);
+    throw err;
+  }
+}
+
+async function addTailwindDependencies(projectPath) {
+  const packageJsonPath = join(projectPath, 'package.json');
+  try {
+    const packageJson = await readFile(packageJsonPath, 'utf8');
+    const packageData = JSON.parse(packageJson);
+
+    packageData.devDependencies = {
+      ...packageData.devDependencies,
+      "postcss": "^8.4.30",
+      "tailwindcss": "^4.0.12",
+      "@tailwindcss/vite": "^4.0.6",
+    };
+
+    await writeFile(packageJsonPath, JSON.stringify(packageData, null, 2));
+  } catch (err) {
+    console.error('Error adding Tailwind CSS dependencies to package.json:', err);
+    throw err;
+  }
+}
 
 async function createSigJsApp() {
   const answers = await prompt([
@@ -31,6 +67,12 @@ async function createSigJsApp() {
       default: 'base'
     },
     {
+      type: 'confirm',
+      name: 'tailwind',
+      message: 'Do you want to use Tailwind CSS?',
+      default: false
+    },
+    {
       type: 'input',
       name: 'directory',
       message: 'Where do you want to create the project?',
@@ -49,6 +91,32 @@ async function createSigJsApp() {
         return !ignoredFiles.some(file => src.endsWith('/' + file));
       }
     });
+
+    // Copy Tailwind CSS files
+    if (answers.tailwind) {
+      await copy(
+        join(__dirname, '../template/tailwind'),
+        projectPath,
+        {
+          filter: (src, dest) => {
+            return !ignoredFiles.some(file => src.endsWith('/' + file));
+          }
+        }
+      );
+    }
+
+    if (answers.tailwind) {
+      const tailwindTemplatePath = join(__dirname, '../template/tailwind');
+      await copy(tailwindTemplatePath, projectPath, {
+        filter: (src) => {
+          const relativePath = src.replace(tailwindTemplatePath, '');
+          return !ignoredFiles.some(file => relativePath.includes(file));
+        }
+      });
+
+      await addTailwindDependencies(projectPath);
+      await addTailwindToScss(projectPath);
+    }
 
     console.log('Project created successfully at', projectPath);
   } catch (err) {
